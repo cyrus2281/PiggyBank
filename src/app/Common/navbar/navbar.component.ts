127a9c2cd48e7a17bdff7f443d472558394f9fb3
Milad Mobini <miladmobini2@gmail.com>
2021-11-20 16:42:55 -0500

    update nav, created button + animation

--- src/app/Common/navbar/navbar.component.ts
+++ src/app/Common/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import APP_THEME from 'src/app/Core/theme/theme';
 
 @Component({
   selector: 'pg-navbar',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  app_theme = APP_THEME;
 
   constructor() { }
 
