bd58cf65b8a5596b5df52282d69dfa0bfca5d418
Milad Mobini <miladmobini2@gmail.com>
2021-11-28 02:21:45 -0500

    enlarged nav icon buttons

--- src/app/Common/navbar/navbar.component.ts
+++ src/app/Common/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IconSizeEnum } from 'src/app/Components/icon/icons/icon.enum';
 import APP_THEME from 'src/app/Core/theme/theme';
 import { HeaderService } from '../header/header.service';
 
@@ -9,6 +10,7 @@ import { HeaderService } from '../header/header.service';
 })
 export class NavbarComponent implements OnInit {
   app_theme = APP_THEME;
+  iconSize = IconSizeEnum.xxlarge;
 
   constructor(public headerService: HeaderService) { }
 
